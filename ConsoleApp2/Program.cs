7e32535fbacb8de809869b1e9ceacc4c0c948574
FraintSaad <islamqwertyu@mail.ru>
2024-08-15 21:01:46 +0300

    ConsoleApp and Unit-tests

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using TestLibrary.Shapes;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var circle = new Circle(10);
+            Console.WriteLine(circle.CalculateArea());
+
+            var triangle = new Triangle(3, 4, 5 );
+            Console.WriteLine(triangle.CalculateArea());
+            Console.WriteLine(triangle.IsRightAngled());
+        }
+    }
+}
---
+++ TestProject/ShapeTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using TestLibrary.Shapes;
+
+
+namespace TestProject
+{
+    internal class ShapeTests
+    {
+        [TestFixture]
+        public class SomeTests
+        {
+            [Test]
+            public void CircleAreaTest()
+            {
+                var circle = new Circle(5);
+                Assert.AreEqual(Math.PI * 25, circle.CalculateArea(), 1e-10);
+            }
+
+            [Test]
+            public void TriangleAreaTest()
+            {
+                var triangle = new Triangle(3, 4, 5);
+                Assert.AreEqual(6, triangle.CalculateArea(), 1e-10);
+            }
+
+            [Test]
+            public void RightAngledTriangleTest()
+            {
+                var triangle = new Triangle(3, 4, 5);
+                Assert.IsTrue(triangle.IsRightAngled());
+            }
+
+            [Test]
+            public void NotRightAngledTriangleTest()
+            {
+                var triangle = new Triangle(3, 4, 6);
+                Assert.IsFalse(triangle.IsRightAngled());
+            }
+
+        }
+    }
+}
